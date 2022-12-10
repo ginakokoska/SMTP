a5350e3e2fab267c67c35bca13910a47febf6f69
YannickSchall <yannickschall@gmx.de>
2022-12-10 22:37:36 +0100

    .

--- main.py
+++ main.py
@@ -6,46 +6,32 @@ import base64
 # encodeeverythingsendtoserver
 
 
-def ascii_to_base64(ascii_string):  # encode
-    #return ascii_string.decode()
-    return (base64.b64encode(ascii_string.encode('utf-8'))).decode('utf-8')
-
-
-def base64_to_ascii(base64_string):  # decode
-    #return
-    return (base64.b64decode(base64_string)).decode('utf-8')
-
-
-def check_message(msg):
-    if msg.endswith("\r\n"):
-        return msg
-    else:
-        return msg + "\r\n"
-
-
 def client_socket(sock, msg, mailFrom, rcpt):
     sock.send(mailFrom.encode())
-    received_from = ascii_to_base64(sock.recv(1024))
+    received_from = sock.recv(1024).decode()
     print(received_from)
 
     sock.send(rcpt.encode())
-    received_rcpt = ascii_to_base64(sock.recv(1024))
+    received_rcpt = sock.recv(1024).decode()
     print(received_rcpt)
 
     data = "DATA\r\n"
     sock.send(data.encode())
-    received_data = ascii_to_base64(sock.recv(1024))
+    received_data = sock.recv(1024).decode()
     print(received_data)
+    print("data--")
 
     msg = check_message(msg)
     sock.send(msg.encode())
     received_msg = ascii_to_base64(sock.recv(1024))
     print(received_msg)
+    print("msg--")
 
     dot = "."
     sock.send(dot.encode())
     received_dot = ascii_to_base64(sock.recv(1024))
     print(received_dot)
+    print("dot--")
 
     quit = "QUIT\r\n"
     sock.send(quit.encode())
@@ -58,32 +44,36 @@ def client_socket(sock, msg, mailFrom, rcpt):
 
 def login(sock, username, password, mail_server):
     sock.connect(mail_server)
-    recv_mail_server = base64_to_ascii(sock.recv(1024))
+    recv_mail_server = (sock.recv(1024)).decode()
+    print("received mail server:", recv_mail_server)
 
+    ehlo = "EHLO Alice\r\n"
+    sock.send(ehlo.encode())
+    received_ehlo = sock.recv(1024).decode()
+    print("HELO/EHLO return:", received_ehlo)
+    if received_ehlo.startswith('250'):
+        print('250 reply not received from server.')
 
-    username = ascii_to_base64(username)  # ascii_to_base64("\x00{username}")
-    password = ascii_to_base64(password)  # ascii_to_base64("\x00{password}")
-    authPlain = ascii_to_base64("AUTH PLAIN ")
-    authMsg = authPlain + username + password
+    authPlain = "AUTH PLAIN "
+    cred = base64.b64encode(("\x00"+username + "\x00"+password).encode())
+    authMsg = authPlain.encode() + cred + "\r\n".encode()
     sock.send(authMsg)
     received_auth = base64_to_ascii(sock.recv(1024))
     print("send login details to server: " + authMsg)
 
 
-
 if __name__ == '__main__':
-    username = "yannickrechnernetze@gmx.de"
-    password = "GinaYannick"
-    msg = "helloo future"
-    mailFrom = "<MAIL FROM:<yannickrechnernetze@gmx.de>"
-    rcpt = "<RCPT TO:<gi161kok@htwg-konstanz.de>"
-    authPlain = "AUTH PLAIN "
-    mail_server = ("smtp.gmx.com", 25)
+    print("started")
+    username = "rnetin"
+    password = "Ueben8fuer8RN"
+    msg = "helloo future\r\n"
+    mailFrom = "MAIL FROM:<rnetin>\r\n"
+    rcpt = "RCPT TO:<gi161kok@htwg-konstanz.de>\r\n"
+    mail_server = ("asmtp.htwg-konstanz.de", 587)
     clientSocket = socket(AF_INET, SOCK_STREAM)
-
+    print("2")
     login(clientSocket, username, password, mail_server)
     client_socket(clientSocket, msg, mailFrom, rcpt)
-
 
     # Press the green button in the gutter to run the script.
 
